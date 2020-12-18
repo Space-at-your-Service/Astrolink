f0251eec8759727255ba254db701dbfddfd6d623
Vazroc <valvierge@gmail.com>
2020-12-18 17:29:28 +0100

    Added layout elements to Experiments and Experiments datasheets

--- vue/src/views/ExperimentData.vue
+++ vue/src/views/ExperimentData.vue
@@ -1,12 +1,43 @@
 <template>
 	<div class="main-container">
-		<div style="height: 620px; background-color: #fff; color: black; border-radius: 3px; overflow: auto;">
+		<b-container class="p-0">
+			<b-row>
+				<b-col cols=10>
+					<form>
+						<b-form-input id="titleInput" v-model="title" placeholder="Textsheet Title" class="text-center">
+						</b-form-input>
+					</form>
+				</b-col>
+
+				<b-col>
+					<b-button @click="saveContent" class="float-right" size="lg" variant="info" style="border-radius: 15px"><b-icon icon="cloud-upload" ></b-icon> Save</b-button>
+				</b-col>
+			</b-row>
+		</b-container>
+
+		<b-container style="background-color: #fff; color: black; overflow: hidden; border-radius: 0px 0px 0px 15px" class="p-4">
 			<vue-editor v-model="datasheet.content" @imageAdded="handleImageAdded" :editorToolbar="customToolbar" style="height: 1000px;"></vue-editor>
-		</div>
-		<b-button variant="info" block @click="saveContent">Save content</b-button>
-		{{ datasheet.content }}
+		</b-container>
+		<!--  -->
 	</div>
 </template>
+
+<style>
+	#titleInput {
+		border-style: none; 
+		border-radius: 15px 15px 0px 0px; 
+		background-color: rgba(264, 264, 264, 0.2);
+		font-size: 1.8em;
+		letter-spacing: 1px;
+		color: rgba(264, 264, 264, 1);
+
+	}
+
+	#titleInput:focus {
+		font-style: italic;
+		color: rgba(264, 264, 264, 0.8);
+	}
+</style>
 
 <script>
 	import {VueEditor} from 'vue2-editor';
@@ -21,6 +52,7 @@
 		data() {
 			return {
 				datasheet: new Datasheet(),
+				title: "Textsheet Title",
 				customToolbar: [
 					[{ 'size': ['small', false, 'large', 'huge'] }],
 					['bold', 'italic', 'underline', 'strike'],
--- vue/src/views/ExperimentDetails.vue
+++ vue/src/views/ExperimentDetails.vue
@@ -5,37 +5,67 @@
 		footer-tag="footer"  
 		class="text-dark text-center" 
 		>
+			<template #header>
+				<h2 style=" font-variant-caps: small-caps;">{{ experiment.title }}</h2> 
+				<span :class="['badge', 'ml-2', {'badge-success': experiment.state === 'complete'}, {'badge-primary': experiment.state === 'planned'}, {'badge-danger': experiment.state === 'aborted'}]">{{experiment.state}}</span>
+			</template>
 
 			<b-card-img src="https://www.rrts.com/wp-content/uploads/2018/09/Blue-Background.jpg" alt="Experiment image" height="300px" top>
 			</b-card-img>
 			
 			<b-card-text class="p-3">
-				<h4>Description</h4>
-				{{ experiment.info }}
+				<b-container class="mb-2 py-3">
+					<h4>Description</h4>
+					{{ experiment.info }}
+				</b-container>
+				<b-container class="my-2 py-3" style="border-top-style: solid; border-top-width: 1px">
+					<h4>Procedure(s)</h4>
+					<ul>
+						<li v-for="procedure in experiments.procedure"  :key="procedure.nick">
+							<router-link  to="/">
+								{{ procedure.title }}
+							</router-link>
+						</li>
+					</ul>
+				</b-container>
+				<b-container class="my-2 py-3" style="border-top-style: solid; border-top-width: 1px">
+					<h4>Data</h4>
+					<b-row>
+						<b-col cols=6>
+							<h5><b-icon icon="file-earmark-richtext" variant="primary"></b-icon> Textsheets</h5>
+							<ul>
+								<!-- <li v-for="textsheet in experiment.data.textsheets" :key="textsheet.id">
+									<router-link  to="/">
+										{{ textsheet.title }}
+									</router-link>
+								</li> -->
+							</ul>
+							<router-link :to="'/experiments/'+experiment.title+'/newdata'">
+								<b-button class="m-1" variant="primary" size="sm"><b-icon icon="file-earmark-plus"></b-icon> New textsheet</b-button>
+							</router-link>
+						</b-col>
+					
+						<b-col cols=6>
+							<h5><b-icon icon="file-earmark-spreadsheet" variant="success"></b-icon> Spreadsheets</h5>
+							<ul>
+								<!-- <li v-for="spreadsheet in experiment.data.spreadsheets" :key="spreadsheet.id">
+									<router-link  to="/">
+										{{ spreadsheet.title }}
+									</router-link>
+								</li> -->
+							</ul>
 
-				<h4>Protocol</h4>
-				<router-link  to="/">
-							Procedure title	
-				</router-link>
-
-				<h4>Data</h4>
-				<div>
-					<router-link v-for="datasheet in experiment.datasheets" :key="datasheet.id" to="/experiments/exper">
-						<b-button variant="info" class="m-1" >Datasheet {{ datasheet.id }}</b-button>
-					</router-link>
-				</div>
+							<router-link to="/">
+								<b-button class="m-1" variant="success" size="sm" disabled><b-icon icon="file-earmark-plus"></b-icon> New spreadsheet</b-button>
+							</router-link>
+						</b-col>
+					</b-row>
+				</b-container>
 			</b-card-text>
 
-			<template #header>
-				<h1 style=" font-variant-caps: all-small-caps;">{{ experiment.title }}</h1> 
-				<span :class="['badge', 'ml-2', {'badge-success': experiment.state === 'complete'}, {'badge-primary': experiment.state === 'planned'}, {'badge-danger': experiment.state === 'aborted'}]">{{experiment.state}}</span>
-			</template>
-
 			<template #footer>
-				<router-link v-for="datasheet in experiment.datasheets" :key="datasheet.id" :to="'/experiments/'+experiment.title+'/newdata'">
-					<b-button variant="info" class="m-1" >Datasheet {{ datasheet.id }}</b-button>
-				</router-link>
-		</template>
+				<b-button variant="info" disabled><b-icon icon="pencil-square"></b-icon> Edit</b-button>
+			</template>
 		</b-card>
 	</div>
 </template>
