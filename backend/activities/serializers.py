5ce2a93aae02db18ec0a3c0a4feb4c6f36a44b61
holbormon <holbormon@CL4P-TP>
2021-01-08 18:11:40 +0100

    Procedures totally fine and compliant with the new type system

--- backend/activities/serializers.py
+++ backend/activities/serializers.py
@@ -49,9 +49,33 @@ class ProcedureSubtypeSerializer(serializers.HyperlinkedModelSerializer):
 
 class ProcedureSerializer(serializers.HyperlinkedModelSerializer):
 
-    types = ProcedureSubtypeSerializer()
+    type = serializers.CharField(max_length = 50)
+    subtype = serializers.CharField(max_length = 50)
 
     class Meta:
 
         model = Procedure
-        fields = ("nick", "title", "types", "abstract", "pdfFile")
+        fields = ("nick", "title", "type", "subtype", "abstract", "pdfFile")
+
+
+    def create(self, validated_data):
+
+        pmt = ProcedureType.objects.get_or_create(type = validated_data.pop("type"))[0]
+
+        pst = ProcedureSubtype.objects.get_or_create(subtype = validated_data.pop("subtype"), 
+                                                     masterType = pmt)[0]
+
+        validated_data.update({"types" : pst})
+
+        return Procedure.objects.create(**validated_data)
+
+
+    def to_representation(self, instance):
+
+        rep = {"nick" : instance.nick,
+               "title" : instance.title,
+               "type" : instance.types.masterType.type,
+               "subtype" : instance.types.subtype,
+               "abstract" : instance.abstract}
+
+        return rep
