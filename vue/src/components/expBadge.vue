7daa0f2f7ee66b2d07f7826ac1b8e185693e3ac9
Vazroc <valvierge@gmail.com>
2021-01-10 02:10:27 +0100

    [Experiments] Created an experiment badge component

---
+++ vue/src/components/expBadge.vue
@@ -0,0 +1,11 @@
+<template>
+	<span :class="['badge', {'badge-primary': status === 'planned'}, {'badge-warning': status === 'ongoing'}, {'badge-success': status === 'complete'}, {'badge-danger': status === 'aborted'}]">{{status}}
+	</span>
+</template>
+
+<script>
+	export default {
+		name: 'expBadge',
+		props: ['status']
+	}
+</script>
