4b14cdd318174bd963c62bd80fe0dffa3f95c6ad
Vazroc <valvierge@gmail.com>
2021-01-11 00:50:09 +0100

    [Flightplan] Flightplan is no longer routed as 'Home'

--- vue/src/views/Flightplan.vue
+++ vue/src/views/Flightplan.vue
@@ -1,12 +1,15 @@
 <template>
 	<div class="main-container">
 		<h3 class="section-title">Flightplan</h3>	
+		{{ flightplan }}
+
 		<Planning />
 	</div>
 </template>
 
 <script>
 	import Planning from '../components/Planning.vue';
+	import { mapState } from 'vuex'
 
 	export default {
 		components: {
@@ -15,6 +18,9 @@
 		data() {
 			return {
 			}
+		},
+		computed: {
+			...mapState('flightplan', ['flightplan'])
 		}
 	};
 </script>
