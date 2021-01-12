856420779568ff9f89bac620ccac44a72c61301f
holbormon <holbormon@CL4P-TP>
2021-01-12 13:02:16 +0100

    Added /asclepios/group/<g> endpoint

--- backend/asclepios/views.py
+++ backend/asclepios/views.py
@@ -3,7 +3,10 @@ from rest_framework import status
 
 from django.http import JsonResponse
 
-from .serializers import AsclepianSerializer, AsclepianFavoritesSerializer
+from .serializers import AsclepianSerializer, AsclepianFavoritesSerializer, SimpleAsclepianSerializer
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.http.response import HttpResponse
 
 
 class ProfileView(APIView):
@@ -27,6 +30,10 @@ class ProfileView(APIView):
                 ser.save()
                 rep.update(ser.data)
 
+            else:
+
+                rep.update(ser.errors)
+
         if "oldPassword" in request.data and "newPassword" in request.data:
 
             if request.user.check_password(request.data["oldPassword"]):
@@ -44,3 +51,18 @@ class ProfileView(APIView):
             return JsonResponse(rep, status = status.HTTP_202_ACCEPTED)
 
         return JsonResponse(ser.errors, status = status.HTTP_400_BAD_REQUEST)
+
+
+class GroupView(APIView):
+
+    def get(self, request, pk):
+
+        users = get_user_model().objects.filter(groups__name = pk)
+
+        if users:
+
+            ser = SimpleAsclepianSerializer(users, many = True)
+
+            return JsonResponse(ser.data, status = status.HTTP_200_OK, safe = False)
+
+        return HttpResponse(status = status.HTTP_204_NO_CONTENT)
