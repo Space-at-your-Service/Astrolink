8fd825cd931bc798018c233d5dd7abaab0f4b588
holbormon <holbormon@CL4P-TP>
2021-01-13 17:36:02 +0100

    bugfix part 2

--- backend/activities/serializers.py
+++ backend/activities/serializers.py
@@ -160,7 +160,7 @@ class ExperimentSerializer(serializers.ModelSerializer):
                "description" : instance.description,
                "operators" : list(instance.operators.all().values_list("username", flat = True)),
                "supervisor" : instance.supervisor.username,
-               "procedures" : list(instance.procedures.all())}
+               "procedures" : ProcedureSerializer(instance.procedures.all(), many = True).data}
 
         rep["data"] = {"textsheets" : TextsheetSerializer(instance.textsheets.all(), many = True).data,
                        "spreadsheets" : list(instance.spreadsheets.all())}
