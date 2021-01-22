d658f67d1862b92db19959ebd1445a098640141e
holbormon <holbormon@CL4P-TP>
2021-01-22 14:55:32 +0100

    Re-fix deployment branch

--- backend/astrolink/settings.py
+++ backend/astrolink/settings.py
@@ -24,14 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".localhost"]
-
-#The following allows requests from Vue's development server
-#It is therefore NOT needed in production
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-    "http://localhost:8080", 
-)
+ALLOWED_HOSTS = ["astrolink.earth"]
 
 #Security settings
 CSRF_COOKIE_SECURE = True
@@ -49,7 +42,6 @@ INSTALLED_APPS = [
     #Imported apps
     "rest_framework", #Enable Django Rest Framework (DRF)
     "rest_framework.authtoken", #Enable DRF's Token generation
-    "corsheaders", #Enable front-end REST requests in development environment
 
     #Custom apps
     "activities",
@@ -67,9 +59,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    #Imported middleware
-    "corsheaders.middleware.CorsMiddleware",
 
 ]
 
