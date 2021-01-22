4420c69c1855be5e5f169eaa3de2161cafa33a56
holbormon <holbormon@CL4P-TP>
2021-01-22 17:01:05 +0100

    Seperated favorites and password endpoint
    Corrected the fact that in admin view favoriteOf could not be empty

--- backend/asclepios/urls.py
+++ backend/asclepios/urls.py
@@ -7,12 +7,13 @@ of the app's views
 
 from django.urls import path
 
-from .views import ProfileView, UnitsView
+from .views import ProfileView, PasswordView, UnitsView
 
 
 urlpatterns = [
 
     path("profile/", ProfileView.as_view()),
+    path("password/", PasswordView().as_view()),
     path("unit/<str:pk>", UnitsView.as_view())
 
 ]
