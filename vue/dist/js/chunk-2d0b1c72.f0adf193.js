4268d96e878617355164f6bf07635574e47ff971
Vazroc <valvierge@gmail.com>
2021-01-24 12:43:52 +0100

    [DIST] Deleted production build from dev branch

--- vue/dist/js/chunk-2d0b1c72.f0adf193.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b1c72"],{"20fd":function(n,t,e){"use strict";e.r(t);var c=function(){var n=this,t=n.$createElement;n._self._c;return n._m(0)},a=[function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",{staticClass:"main-container"},[e("h3",[n._v("Communication")])])}],i=e("2877"),r={},s=Object(i["a"])(r,c,a,!1,null,null,null);t["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d0b1c72.f0adf193.js.map
