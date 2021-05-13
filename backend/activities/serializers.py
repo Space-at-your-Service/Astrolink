4c2d781b1a04f9ffbee04aaf892596d8b6c24022
holbormon <holbormon@CL4P-TP>
2021-05-13 11:49:18 +0200

    test

--- backend/activities/serializers.py
+++ backend/activities/serializers.py
@@ -110,10 +110,13 @@ class TaskSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
 
         self.fields["procedures"] = ProcedureSerializer(many = True)
-        self.fields["split"] = self.fields.pop("holder")
         self.fields["class"] = self.fields.pop("category")
 
-        return serializers.ModelSerializer.to_representation(self, instance)
+        rep = serializers.ModelSerializer.to_representation(self, instance)
+
+        rep["split"] = instance.holder
+
+        return rep
 
 
 class TextsheetSerializer(serializers.ModelSerializer):
