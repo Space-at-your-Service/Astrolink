f1d060f8ea2963b89fb8b4c1114a36ed5b7e45c7
MaximeFth <maxime.fellrath@gmail.com>
2021-06-25 11:59:42 +0200

    fix socket.io

--- vue/dist/js/chunk-2d0e24e5.b952e443.js
+++ vue/dist/js/chunk-2d0e24e5.b952e443.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e24e5"],{"7dac":function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},a=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"main-container"},[n("h1",{staticClass:"text-center",staticStyle:{"font-family":"sans-serif"}},[t._v("404 NOT FOUND")])])}],c=n("2877"),i={},l=Object(c["a"])(i,s,a,!1,null,null,null);e["default"]=l.exports}}]);
-//# sourceMappingURL=chunk-2d0e24e5.1e14272b.js.map
+//# sourceMappingURL=chunk-2d0e24e5.b952e443.js.map
